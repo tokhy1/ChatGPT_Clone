d971b3db7b4cd69b54da8b4901870b19fd068fc0
tokhy24 <mohamed21346587@gmail.com>
2023-10-20 20:06:13 +0300

    initial commit:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import upgrade from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
+import gptImageLogo from "./assets/chatgptLogo.svg";
+
+import { sendMessageToOpenAi } from "./openAi";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const msgEnd = useRef(null);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      text: "Hi, i'm TokhyGPT",
+      isBot: true,
+    },
+  ]);
+
+  useEffect(() => {
+    msgEnd.current.scrollIntoView();
+  }, [messages]);
+
+  const handleSend = async () => {
+    const text = input;
+    setInput("");
+    setMessages([...messages, { text, isBot: false }]);
+
+    const res = await sendMessageToOpenAi(text);
+    setMessages([
+      ...messages,
+      { text, isBot: false },
+      { text: res, isBot: true },
+    ]);
+  };
+
+  const handleEnter = async (e) => {
+    if (e.key == `Enter`) await handleSend();
+  }; // add to input onKeyDown
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="Logo" className="logo" />
+            <span className="brand">TokhyGPT</span>
+          </div>
+
+          <button
+            className="midBtn"
+            onClick={() => {
+              window.location.reload();
+            }}>
+            <img src={addBtn} alt="New Chat" className="addBtn" />
+            New Chat
+          </button>
+
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              Greeting
+            </button>
+
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              Introduction to React.js
+            </button>
+          </div>
+        </div>
+
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="home" className="listItemsImg" />
+            Home
+          </div>
+
+          <div className="listItems">
+            <img src={saved} alt="saved" className="listItemsImg" />
+            Saved
+          </div>
+
+          <div className="listItems">
+            <img src={upgrade} alt="upgrade" className="listItemsImg" />
+            Upgrade to Pro
+          </div>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img src={userIcon} alt="" className="chatImg" />
+            <p className="text">hey, how are you?</p>
+          </div>
+
+          <div className="chat bot">
+            <img src={gptImageLogo} alt="" className="chatImg" />
+            <p className="text">I'm fine!, how can i assist you today?</p>
+          </div>
+
+          {/* {messages.map((message, i) => (
+            <div className={message.isBot ? "chat bot" : "chat"} key={i}>
+              <img
+                src={message.isBot ? gptImageLogo : userIcon}
+                alt=""
+                className="chatImg"
+              />
+              <p className="text">{message.text}</p>
+            </div>
+          ))} */}
+
+          <div ref={msgEnd}></div>
+        </div>
+
+        <div className="chatFooter">
+          <div className="inp">
+            <input
+              type="text"
+              placeholder="Send a Message"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <button className="send">
+              <img src={sendBtn} alt="send" />
+            </button>
+          </div>
+          <p>
+            TokhyGPT May produce inaccurate information about people, places, or
+            facts. TokhyGPT September 25 Version.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/openAi.js
@@ -0,0 +1,21 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: "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"
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function sendMessageToOpenAi(message) {
+    const response = await openai.createCompletion({
+        engine: 'text-davinci-003',
+        prompt: message,
+        max_tokens: 32,
+        n: 1,
+        stop: '.',
+        temperature: 0.5,
+    });
+    return response.data.choices[0].text
+}
+
+export { sendMessageToOpenAi };
